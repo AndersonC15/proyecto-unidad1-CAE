0446179022ec60a058fd4a8baccee14759847f94
Pedro27-J <pedro.l.jimenez@unl.edu.ec>
2025-11-05 23:40:05 -0500

    Agrego de la Clase NodoPila

--- estructuradedatosUnidad1/src/main/java/estructuras/pila/NodoPila.java
+++ estructuradedatosUnidad1/src/main/java/estructuras/pila/NodoPila.java
@@ -2,16 +2,6 @@ package estructuras.pila;
 
 import modelo.Accion;
 
-public class NodoPila {
-    private Accion dato;
-    private NodoPila siguiente; // Usar 'siguiente' para consistencia con otras estructuras
-
-    // Constructor corregido para enlazar al crear (conveniente para el push)
-    public NodoPila(Accion dato, NodoPila siguiente) {
-        this.dato = dato;
-        this.siguiente = siguiente;
-    }
-
-    public Accion getDato() { return dato; }
-    public NodoPila getSiguiente() { return siguiente; }
+public record NodoPila(Accion dato, NodoPila siguiente) {
+    
 }
